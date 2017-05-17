5d9ac5a23bda381d50d6d9b6e2e0474097301353
Yaroslav <slava.guk@mail.ru>
2017-05-17 16:13:12 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+var name = prompt('What is your name?', 'Yaroslav');
+var event = prompt('What are the event?', 'meeting');
+var time = prompt('When you want to go?', '21:00');
+var place = prompt('Wheare you want to go?', 'Lviv');
+alert(name + ' ' + 'has a ' + event + ' today at ' + time + ':' + ' somewhere in '+ place);
+console.log(name + ' ' + 'has a ' + event + ' today at ' + time + ':' + ' somewhere in '+ place);
