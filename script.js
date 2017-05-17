c80477bb7d71916f79fdd01603437159bcc95163
Yaroslav <gukyaroslav87@gmail.com>
2017-05-17 22:46:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var name = prompt('What is your name?', '');
-var event = prompt('What kind of event you want to attend?', '');
+var Event = prompt('What kind of event you want to attend?', '');
 var time = prompt('When you want to go?', '');
 var place = prompt('Wheare you want to go?', '');
-alert(name + ' ' + 'has a ' + event + ' today at ' + time + ' ' + ' somewhere in '+ place);
-console.log(name + ' ' + 'has a ' + event + ' today at ' + time + ' ' + ' somewhere in '+ place);
+alert(name + ' ' + 'has a ' + Event + ' today at ' + time + ' ' + ' somewhere in '+ place);
+console.log(name + ' ' + 'has a ' + Event + ' today at ' + time + ' ' + ' somewhere in '+ place);
