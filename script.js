9c5c0bc8dda3b0d876e9b5ce8d5dd6d8358242af
Yaroslav <gukyaroslav87@gmail.com>
2017-05-17 21:32:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var name = prompt('What is your name?', 'Yaroslav');
-var event = prompt('What are the event?', 'meeting');
-var time = prompt('When you want to go?', '21:00');
-var place = prompt('Wheare you want to go?', 'Lviv');
+var name = prompt('What is your name?', '');
+var event = prompt('What are the event?', '');
+var time = prompt('When you want to go?', '');
+var place = prompt('Wheare you want to go?', '');
 alert(name + ' ' + 'has a ' + event + ' today at ' + time + ' ' + ' somewhere in '+ place);
 console.log(name + ' ' + 'has a ' + event + ' today at ' + time + ' ' + ' somewhere in '+ place);
