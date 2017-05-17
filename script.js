10ffef2baf8f717449b85461664b3f1fefe9a1a7
Yaroslav <gukyaroslav87@gmail.com>
2017-05-17 22:40:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var name = prompt('What is your name?', '');
-var event = prompt('What are the event?', '');
+var event = prompt('What kind of event you want to attend?', '');
 var time = prompt('When you want to go?', '');
 var place = prompt('Wheare you want to go?', '');
 alert(name + ' ' + 'has a ' + event + ' today at ' + time + ' ' + ' somewhere in '+ place);
